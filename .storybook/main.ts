a2f443f7481d51a3bee907d132e70192c7ef1ca8
Goulven <71071559+GoulvenF@users.noreply.github.com>
2025-06-16 00:35:14 +0200

    fix: enable storybook scripts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,12 @@
-import { defineNuxtConfig } from 'nuxt/config'
-import { createRequire } from 'module'
+import type { StorybookConfig } from '@storybook-vue/nuxt'
 
-// Storybook's Vue preset expects a browser-like global `window` object even
-// when executed in Node during Nuxt's module initialization. Creating a stub
-// avoids `STORYBOOK_ENV` assignment errors when starting the dev server.
-const g = globalThis as unknown as { window?: unknown }
-if (typeof g.window === 'undefined') {
-  g.window = g
-}
-
-const require = createRequire(import.meta.url)
-const { nuxt } = require('@storybook-vue/nuxt')
-
-export default defineNuxtConfig({
+const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|ts|mdx)'],
   addons: ['@storybook/addon-essentials'],
-  extends: [nuxt()],
-})
+  framework: {
+    name: '@storybook-vue/nuxt',
+    options: {},
+  },
+}
+
+export default config
