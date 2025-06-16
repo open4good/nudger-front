283781eda81330b66e3ff7f0d6500e5bcacd3c94
GoulvenF <GoulvenF@users.noreply.github.com>
2025-06-16 16:00:23 +0000

    deploy: 668960f7bb67bf514b0e15656469cbfd58b94073

---
+++ storybook/_nuxt/BabTWnYm.js
@@ -0,0 +1,11 @@
+import{_ as a}from"./CJpG5RoY.js";import"./B_uKG6tn.js";const r={component:a,title:"ListBox"},i=[{icon:"a.png",name:"Aid",description:"Association A"},{icon:"b.png",name:"Benefit",description:"Association B"}],e={render:()=>({components:{ListBox:a},template:'<ListBox :items="items" />',data:()=>({items:i})})};var t,s,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      ListBox
+    },
+    template: '<ListBox :items="items" />',
+    data: () => ({
+      items: sample
+    })
+  })
+}`,...(o=(s=e.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,r as default};
